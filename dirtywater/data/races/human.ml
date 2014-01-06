b3db54de849fdea2c0dec19f5308cc2951bc2c72
sproctor <sproctor@gmail.com>
2014-01-05 20:29:29 -0500

    added example race in ml

---
+++ dirtywater/data/races/human.ml
@@ -0,0 +1,7 @@
+new race "normalhuman" "human"
+  (new bodypartDef Head [
+    new bodypartDef Torso [
+      new bodypartDef (Arm Left) [new bodypartDef (Hand Left) []];
+      new bodypartDef (Arm Right) [new bodypartDef (Hand Right) []];
+      new bodypartDef (Leg Left) [new bodypartDef (Foot Left) []];
+      new bodypartDef (Leg Right) [new bodypartDef (Foot Right) []]]]);;
