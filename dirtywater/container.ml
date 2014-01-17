58f878265bc06d88bdb3de8122562be959a427b5
Sean Proctor <sproctor@gmail.com>
2014-01-17 10:06:31 -0500

    display items behind/under other items.

--- dirtywater/container.ml
+++ dirtywater/container.ml
@@ -77,11 +77,24 @@ class virtual simple_container =
     method get_contents (where : position option) : tangible list =
       match where with
       | Some pos ->
-          let compare_pos (p, t) =
-            if p = pos then Some t
-            else None
+          let rec relative_to thing =
+            let rec relative_helper content_list =
+               match content_list with
+               | (Under t, other)::rest when t == thing ->
+                   other::(relative_to other)@(relative_helper rest)
+               | (Behind t, other)::rest when t == thing ->
+                   other::(relative_to other)@(relative_helper rest)
+               | _::rest -> relative_helper rest
+               | [] -> []
+             in relative_helper contents
           in
-          map_some compare_pos contents
+          let rec contents_helper content_list =
+            match content_list with
+            | (p, thing)::rest when p = pos ->
+                thing::(relative_to thing)@(contents_helper rest)
+            | _::rest -> contents_helper rest
+            | [] -> []
+          in contents_helper contents
       | None -> List.map (fun (_, t) -> t) contents
 
     method view_contents (looker : creature) (where : position option)
