7e07368a0813729ddf32c8b31b488f160056414c
Sean Proctor <sproctor@gmail.com>
2014-06-15 22:21:53 -0400

    switch to haskell

--- data/locations/start_init.lua
+++
@@ -1,4 +0,0 @@
-purge ()
-local table = add_tangible ("table")
-local plate = container_add_on (table, "plate")
-item_add_under (plate, "mat")
--- data/races/human.ml
+++
@@ -1,7 +0,0 @@
-open Race
-open Types
-open Race_collection
-
-let () =
-  let r = new humanoid_race "human" in
-  races#add "human" r
