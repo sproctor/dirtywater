64a8836f36964201d8ace3d2a60b00461a969413
Sean Proctor <sproctor@gmail.com>
2014-07-23 14:07:54 -0400

    Fixed remaining issues.

--- src/controller.hs
+++ src/controller.hs
@@ -1,51 +1,56 @@
 import Control.Concurrent.Chan
 import Control.Concurrent.STM.TBQueue
-import Control.Monad
+import Control.Monad.STM
 import System.IO
 
-data Command = Look | Exit | Move Direction
+data Command = Look | Exit | Move Direction deriving (Show, Eq)
 
-data Direction = North | East | South | West
+data Direction = North | East | South | West deriving (Show, Eq)
 
 data Character = Pc | Npc
 
-data Connection = Connection { handle :: Handle, queue :: TBQueue Command , character :: Character }
+data Connection = Connection { handle :: Handle, queue :: TBQueue Command, character :: Character }
 
 data ServerStatus = Running | Stopping
 
-data GameState = GameState { controllers :: [Controller], status :: ServerStatus }
+data GameState = GameState { connections :: [Connection], status :: ServerStatus }
 
-class Controller c where
+{- class Controller c where
   getCommand :: c -> IO (Maybe Command)
   putOutput :: c -> String -> IO ()
   getCharacter :: c -> Character
 
 instance Controller Connection where
-  getCommand (Connection _ queue _) =
-    if null queue
-      then Nothing
-      else Just (head queue)
-  putOutput (Connection handle _ _) =
-    hPutStr handle
-  getCharacter (Connection _ _ c) = c
+-}
+getCommand :: Connection -> IO (Maybe Command)
+getCommand (Connection _ q _) =
+  atomically (tryReadTBQueue q)
+putOutput :: Connection -> String -> IO ()
+putOutput (Connection h _ _) =
+  hPutStr h
+-- getCharacter :: Connection -> Character
+-- getCharacter (Connection _ _ c) = c
 
 mainServer gameState = do
-  let controller : remainingControllers = gameState.controllers
-  cmd <- getCommand controller
+  let (connection : remainingConnections) = connections gameState
+  cmd <- getCommand connection
   let
-    newControllers :: [Controller]
-    newControllers =
-      if cmd == Exit
-        then remainingControllers
-        else remainingControllers ++ controller
-    intermediateGameState = GameState newControllers status
+    newConnections :: [Connection]
+    newConnections = remainingConnections ++ [connection]
     newGameState =
       case cmd of
-        Just realCommand -> do
-          print ((getCharacter controller), realCommand)
-          doACommand (getCharacter controller) realCommand intermediateGameState
-        Nothing -> intermediateGameState
-  mainServer controllers newGameState
+        Just realCommand ->
+          --do
+          let
+            cs =
+              if realCommand /= Exit
+                then newConnections
+                else remainingConnections
+            --print ((getCharacter connection), realCommand)
+          in
+            doCommand (character connection) realCommand (GameState cs (status gameState))
+        Nothing -> GameState newConnections (status gameState)
+  mainServer newGameState
 
-doACommand :: Character -> Command -> GameState -> GameState
-doACommand _ _ gs = gs
+doCommand :: Character -> Command -> GameState -> GameState
+doCommand _ _ gs = gs
