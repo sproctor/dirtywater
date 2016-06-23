4ad89e78b1d80b1a16146d88217676b00e353b3d
sproctor <sproctor@gmail.com>
2016-06-23 16:09:31 -0400

    Added tentative lua examples

---
+++ data/items/shortsword.lua
@@ -0,0 +1,6 @@
+addItem({
+	id = "shortsword",
+	name = "shortsword",
+	short_description = "a shortsword",
+	long_desc = "A very generic shortsword with no special properties at all. It should probably be melted down into something better."
+])
